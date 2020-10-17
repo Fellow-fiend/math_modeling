e3ec9b2a728e0a2af032e5f5996d00a6fdba33f2
Fellow-fiend <72641329+Fellow-fiend@users.noreply.github.com>
2020-10-17 13:34:54 +0200

    lecture first

---
+++ lec_1_list.py
@@ -0,0 +1,17 @@
+q = [124, 'asfsd', 2134]
+
+b = [q, 12, q[1]]
+
+a = [[['sad']], [['story']], [[' , man']]]
+print(q)
+print(b)
+print(q+b)
+print(q+b)
+print(q*3)
+a.append(a)
+a.append(a)
+b = a
+b.append(b)
+a.append(b)
+print(a)
+print('\n', b)
---
+++ lec_1_string.py
@@ -0,0 +1,12 @@
+import os
+a = "string"
+b = "String"
+
+print(a + b)
+
+print(a*3)
+
+print(3*a)
+
+
+os.system("pause")
