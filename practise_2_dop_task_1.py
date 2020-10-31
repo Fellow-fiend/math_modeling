9da958d6a9292747c1f862f9902d7ca190c54526
Fellow-fiend <72641329+Fellow-fiend@users.noreply.github.com>
2020-10-31 13:14:06 +0200

    Update practise_2_dop_task_1.py

--- practise_2_dop_task_1.py
+++ practise_2_dop_task_1.py
@@ -1,6 +1,6 @@
 a = int(input())
 b = int(input())
 c = int(input())
-x1 = -b/2 + (b**2 - 4*a*c)**0.5
-x2 = -b/2 - (b**2 - 4*a*c)**0.5
+x1 = -b/(2*a) + (b**2 - 4*a*c)**0.5 / (2*a) 
+x2 = -b/(2*a) - (b**2 - 4*a*c)**0.5 / (2*a)
 print(x1, '\n', x2)
