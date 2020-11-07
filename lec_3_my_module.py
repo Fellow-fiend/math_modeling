28c053883cf7bf4db3c33c6f13b6081a4450b48b
Fellow-fiend <72641329+Fellow-fiend@users.noreply.github.com>
2020-11-07 13:54:50 +0200

    Add files via upload

---
+++ lec_3_my_module.py
@@ -0,0 +1,7 @@
+попробуйизменитьа = 5
+
+def factorial(n):
+  temp = 1;
+  for i in range(1, n+1, 1):
+    temp *= i
+  return temp
---
+++ main.py
@@ -0,0 +1,11 @@
+import lec_3_my_module as пшелвон
+from lec_3_my_module import попробуйизменитьа
+from lec_3_my_module import factorial
+print(factorial(int(input())))
+print(попробуйизменитьа)
+попробуйизменитьа = 10
+print(попробуйизменитьа)
+from ei_ti_kto import *
+from ei_ti_kto import дать_пинка as пшёлвон
+дать_пинка("Ты", "туда")
+пшёлвон("челодой моловек", "⚰️ ")
